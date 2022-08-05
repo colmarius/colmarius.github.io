d183d51cba9ac7562926528cb55bd0f2a6366ef6
Marius Colacioiu <marius.colacioiu@gmail.com>
2022-08-05 18:35:06 +0200

    Add placeholder image for now

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,16 @@
+import placeholder from '../assets/placeholder.png';
+
 export const HomePage = () => (
   <div className="leading-10 text-3xl antialiased font-light max-w-screen-sm">
     Hi there! I’m Marius Colacioiu – <span className="font-medium">Engineering Leader</span> and{' '}
     <span className="font-medium">Entrepreneur</span> from Zürich, Switzerland.
+    <img
+      src={placeholder}
+      width="342"
+      height="396"
+      alt="Marius Colacioiu"
+      title="Marius Colacioiu"
+      itemProp="image"
+    ></img>
   </div>
 );
