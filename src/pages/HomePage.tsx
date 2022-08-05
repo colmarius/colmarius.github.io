3e6c876d6c5cf543b901c5aa0fcc8303f8ecc35f
Marius Colacioiu <marius.colacioiu@gmail.com>
2022-08-05 18:40:32 +0200

    Add some margin on larger screens

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import placeholder from '../assets/placeholder.png';
 
 export const HomePage = () => (
-  <div className="leading-10 text-3xl antialiased font-light max-w-screen-sm">
-    Hi there! I’m Marius Colacioiu – <span className="font-medium">Engineering Leader</span> and{' '}
-    <span className="font-medium">Entrepreneur</span> from Zürich, Switzerland.
+  <div className="lg:mx-12">
+    <div className="leading-10 text-3xl antialiased font-light max-w-screen-sm mb-8">
+      Hi there! I’m Marius Colacioiu – <span className="font-medium">Engineering Leader</span> and{' '}
+      <span className="font-medium">Entrepreneur</span> from Zürich, Switzerland.
+    </div>
     <img
       src={placeholder}
       width="342"
