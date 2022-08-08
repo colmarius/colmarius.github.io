22958f0fe2b0cb5d4de23e7bba46494463675abd
Marius Colacioiu <marius.colacioiu@gmail.com>
2022-08-08 23:53:53 +0200

    Add ViewMyFullCv section

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -13,15 +13,23 @@ const AboutMeMessage = () => (
   </div>
 );
 
-const Section = ({ title, children }: { title: string; children: ReactNode }) => (
-  <div className="mb-20">
+const Section = ({
+  title,
+  children,
+  className,
+}: {
+  title: string;
+  children: ReactNode;
+  className: string;
+}) => (
+  <div className={className}>
     <h1 className="text-3xl mb-6">{title}</h1>
     <div className="text-lg font-extralight">{children}</div>
   </div>
 );
 
 const WorkExperience = () => (
-  <Section title="Work Experience">
+  <Section title="Work Experience" className="mb-20">
     {ABOUT_ME.WORK_EXPERIENCE.map((work, index) => (
       <div key={index} className="mb-6">
         <p>{work.period}</p>
@@ -40,7 +48,7 @@ const WorkExperience = () => (
 );
 
 const Education = () => (
-  <Section title="Education">
+  <Section title="Education" className="mb-10">
     {ABOUT_ME.EDUCATION.map((education, index) => (
       <div key={index} className="mb-6">
         <p>{education.period}</p>
@@ -51,10 +59,36 @@ const Education = () => (
   </Section>
 );
 
+const ArrowRight = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6 ml-1"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={1.5}
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+  </svg>
+);
+
+const ViewMyFullCv = () => (
+  <a
+    className="flex text-medium hover:text-indigo-600 mb-20"
+    href="https://marius-colacioiu.com/cv/"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <strong className="text-xl">View my full CV</strong> <ArrowRight />
+  </a>
+);
+
 export const AboutPage = () => (
   <Page title="About">
     <AboutMeMessage />
     <WorkExperience />
     <Education />
+    <hr className="w-40 bg-indigo-600 mb-5" />
+    <ViewMyFullCv />
   </Page>
 );
