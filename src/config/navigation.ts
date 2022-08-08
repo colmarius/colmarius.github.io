c8ba72e5d11f90815e7907334e29ecfec08a87d5
Marius Colacioiu <marius.colacioiu@gmail.com>
2022-08-08 22:26:11 +0200

    Hide work and resources pages for now since are in WIP

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -2,7 +2,7 @@ import { NavItemProps } from '@types';
 
 export const NAVIGATION_ITEMS: NavItemProps[] = [
   { link: '/about', title: 'About' },
-  { link: '/work', title: 'Work' },
-  { link: '/resources', title: 'Resources' },
+  // { link: '/work', title: 'Work' },
+  // { link: '/resources', title: 'Resources' },
   { link: '/contact', title: 'Contact' },
 ];
