3a7a65da5e85f8b2d930a92a9f0973672a53d538
Marius Colacioiu <marius.colacioiu@gmail.com>
2022-08-13 17:40:09 +0200

    Extract gtag function

---
+++ src/config/gtag.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable fp/no-arguments */
+/* eslint-disable prefer-rest-params */
+
+declare global {
+  interface Window {
+    dataLayer: any;
+  }
+}
+
+window.dataLayer = window.dataLayer || [];
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function gtag(_: string, __: unknown) {
+  window.dataLayer.push(arguments);
+}
--- src/config/setupGoogleAnalytics.tsx
+++ src/config/setupGoogleAnalytics.tsx
@@ -1,21 +1,8 @@
-/* eslint-disable fp/no-arguments */
-/* eslint-disable prefer-rest-params */
-
-declare global {
-  interface Window {
-    dataLayer: any;
-  }
-}
+import { gtag } from './gtag';
 
 export const setupGoogleAnalytics = () => {
   const configKey = import.meta.env.VITE_GOOGLE_ANALYTICS_KEY;
   if (!configKey) return;
-
-  window.dataLayer = window.dataLayer || [];
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  function gtag(_: string, __: unknown) {
-    window.dataLayer.push(arguments);
-  }
   gtag('js', new Date());
   gtag('config', configKey);
 };
