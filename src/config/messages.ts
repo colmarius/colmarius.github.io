6a3601aee6f559bae9320608430d05d41af0bd52
Marius Colacioiu <marius.colacioiu@gmail.com>
2023-04-10 10:42:49 +0200

    Update current work place

--- src/config/messages.ts
+++ src/config/messages.ts
@@ -1,6 +1,6 @@
 const HOME_PAGE = [
   'For the past 10 years, I have been busy working in the startup world building products in HR-tech and Fin-tech. Several times, I have been one of the first employees in newly born startups having a big impact on the Engineering culture. With a background in computer science, I specialize in building web products from scratch.',
-  'Currently, I’m focusing on building Ledgy as an Engineer, Manager and Leader.',
+  'Currently, I’m focusing on building Norm together with a highly talented team, on a mission to make living energy efficient.',
 ];
 
 export const MESSAGES = Object.freeze({
