eab09e87c3075c2b10f4998cdd5808a47433846e
Marius Colacioiu <marius.colacioiu@gmail.com>
2023-06-07 22:51:30 +0200

    Fix typo

--- src/config/aboutMe.ts
+++ src/config/aboutMe.ts
@@ -5,7 +5,7 @@ const LINKEDIN = 'https://www.linkedin.com/in/mariuscolacioiu/';
 const MESSAGE = [
   'I started coding at a young age, initially for school projects having been in both middle school and high school in classes having Informatics in the curriculum.',
   'My big passion for programming developed during my Computer Science bachelor at the University of Milan, after doing advanced programming and security courses, and having discovering the open source movement and the Linux operating system.',
-  'I got hooked on startups since 2012, after following one of this first YC Startup School online courses. Since then I have joined several startups as one of the first employees which got modest successes (either an exit or IPO). Very early in my career I got lucky and worked for Gild a company based in San Francisco, where I have done my onboarding and got contaminated with the Silicon Valley startup mindset.',
+  'I got hooked on startups since 2012, after following one of the first YC Startup School online courses. Since then I have joined several startups as one of the first employees which got modest successes (either an exit or IPO). Very early in my career I got lucky and worked for Gild a company based in San Francisco, where I have done my onboarding and got contaminated with the Silicon Valley startup mindset.',
   'Since then I have always pushed myself to continue learning and find environments where I can learn and contribute the most. It gives me great joy being part of an amazing startup environment with great mission, team and culture.',
 ];
 
