8637a7084f5fc673392c7a58fff43c2486d6d0df
Marius Colacioiu <marius.colacioiu@gmail.com>
2023-11-10 11:08:07 +0100

    Change years of experience

--- src/config/messages.ts
+++ src/config/messages.ts
@@ -1,5 +1,5 @@
 const HOME_PAGE = [
-  'For the past 10 years, I have been busy working in the startup world building products in HR-tech and Fin-tech. Several times, I have been one of the first employees in newly born startups having a big impact on the Engineering culture. With a background in computer science, I specialize in building web products from scratch.',
+  'For the past 12 years, I have been busy working in the startup world building products in HR-tech and Fin-tech. Several times, I have been one of the first employees in newly born startups having a big impact on the Engineering culture. With a background in computer science, I specialize in building web products from scratch.',
   'Currently, I’m focusing on building Norm together with a highly talented team, on a mission to make living energy efficient.',
 ];
 
