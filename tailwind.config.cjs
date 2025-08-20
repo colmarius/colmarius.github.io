9debacc20a95f605213e8fb7f51188822147d661
Marius Colacioiu <marius.colacioiu@gmail.com>
2025-08-20 19:43:27 +0200

    Fix BaseLayout and update about page: use type instead of interface

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					500: "#6366f1", // indigo-500
+					600: "#4f46e5", // indigo-600
+					700: "#3730a3", // indigo-700
+				},
+			},
+		},
 	},
 	plugins: [],
 };
