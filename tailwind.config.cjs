24d43560c7bbe21bea62cef9b1379bc425063928
Marius Colacioiu <marius.colacioiu@gmail.com>
2025-10-11 09:54:53 +0200

    Add remaining tailwind brand colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,17 @@ module.exports = {
     extend: {
       colors: {
         primary: {
+          50: '#eef2ff', // indigo-50
+          100: '#e0e7ff', // indigo-100
+          200: '#c7d2fe', // indigo-200
+          300: '#a5b4fc', // indigo-300
+          400: '#818cf8', // indigo-400
           500: '#6366f1', // indigo-500
           600: '#4f46e5', // indigo-600
-          700: '#3730a3', // indigo-700
+          700: '#4338ca', // indigo-700
+          800: '#3730a3', // indigo-800
+          900: '#312e81', // indigo-900
+          950: '#1e1b4b', // indigo-950
         },
       },
     },
