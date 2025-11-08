eef21773126b616f24dda0b75ce8cf7c06c69272
Marius Colacioiu <marius.colacioiu@gmail.com>
2025-11-08 10:54:19 +0100

    Style fixes

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -5,6 +5,6 @@ export const formatDate = (date: Date): string => {
   return new Date(date).toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'short',
-    day: 'numeric'
+    day: 'numeric',
   });
 };
